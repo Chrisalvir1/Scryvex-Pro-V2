f156caf5b3c4c1601e7fe7d75933ec80d8020eec
Chrisalvir1 <chrisalvir01@gmail.com>
2026-06-30 12:52:39 -0600

    TS7 compat: remove redundant .mjs fallback

--- tools/typescript-compat/register-fallback.mjs
+++
@@ -1,6 +0,0 @@
-export async function resolve(specifier, context, nextResolve) {
-    if (specifier === 'typescript') {
-        return nextResolve('typescript-js', context);
-    }
-    return nextResolve(specifier, context);
-}
